170cd7b8d329ed24381e06cba5283543fafa8349
Shia <gb@amsg.tk>
2017-10-18 15:50:52 +0800

    fix node-gyp fails
    rollback to request@2.81.0

--- scripts/linux/setup-env.sh
+++ scripts/linux/setup-env.sh
@@ -10,5 +10,9 @@ sudo chown ${USER} /etc/
 
 sudo npm install -g forever userdown wait-for-mongo node-gyp
 
+# fix node-gyp fails
+cd /usr/lib/node_modules/node-gyp
+npm install --save request@2.81.0
+
 # Creating a non-privileged user
 sudo useradd meteoruser || :
